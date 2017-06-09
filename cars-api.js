0c060328be3b332f99e02f9825655f8431775f75
avermeulen <andre.vermeulen@gmail.com>
2017-06-09 06:38:59 +0200

    added car price

--- cars-api.js
+++ cars-api.js
@@ -18,17 +18,24 @@ function randomNumber(max){
   return Math.floor(Math.random() * max);
 }
 
+function randomNumberNotLessThan(lowest, range){
+    return lowest + randomNumber(range);
+}
+
+
 function createCar(){
 
     const make = makes[randomNumber(4)];
     const model = models[make][randomNumber(4)];
     const color = colors[randomNumber(4)];
     const reg_number = regNumbers[randomNumber(6)] + ' ' + randomNumber(100000);
+    const price = randomNumberNotLessThan(50000, 100000)
 
     return {
       make,
       model,
       color,
+      price,
       reg_number
     };
 
